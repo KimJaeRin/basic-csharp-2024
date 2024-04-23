7c94ae847db53f9f8ac294609b435d57f871f3a9
KimJaeRin <158007433+KimJaeRin@users.noreply.github.com>
2024-04-24 08:58:23 +0900

    Update Form1.Designer.cs

--- day06/cs07_Toyproject2/Toyproject2/Form1.Designer.cs
+++ day06/cs07_Toyproject2/Toyproject2/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Toyproject2
+namespace Toyproject2
 {
     partial class Form1
     {
@@ -29,22 +29,6 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            button9 = new Button();
-            button10 = new Button();
-            button11 = new Button();
-            button12 = new Button();
-            button13 = new Button();
-            button14 = new Button();
-            button15 = new Button();
-            button16 = new Button();
             SuspendLayout();
             // 
             // groupBox1
@@ -56,171 +40,11 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "groupBox1";
             // 
-            // button1
-            // 
-            button1.Location = new Point(3, 192);
-            button1.Name = "button1";
-            button1.Size = new Size(76, 68);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(3, 118);
-            button2.Name = "button2";
-            button2.Size = new Size(76, 68);
-            button2.TabIndex = 1;
-            button2.Text = "button1";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(167, 192);
-            button3.Name = "button3";
-            button3.Size = new Size(76, 68);
-            button3.TabIndex = 1;
-            button3.Text = "button1";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(85, 192);
-            button4.Name = "button4";
-            button4.Size = new Size(76, 68);
-            button4.TabIndex = 1;
-            button4.Text = "button1";
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(167, 266);
-            button5.Name = "button5";
-            button5.Size = new Size(76, 68);
-            button5.TabIndex = 1;
-            button5.Text = "button1";
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(85, 266);
-            button6.Name = "button6";
-            button6.Size = new Size(76, 68);
-            button6.TabIndex = 1;
-            button6.Text = "button1";
-            button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            button7.Location = new Point(3, 266);
-            button7.Name = "button7";
-            button7.Size = new Size(76, 68);
-            button7.TabIndex = 1;
-            button7.Text = "button1";
-            button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            button8.Location = new Point(85, 118);
-            button8.Name = "button8";
-            button8.Size = new Size(76, 68);
-            button8.TabIndex = 1;
-            button8.Text = "button1";
-            button8.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            button9.Location = new Point(167, 118);
-            button9.Name = "button9";
-            button9.Size = new Size(76, 68);
-            button9.TabIndex = 1;
-            button9.Text = "button1";
-            button9.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            button10.Location = new Point(167, 340);
-            button10.Name = "button10";
-            button10.Size = new Size(76, 68);
-            button10.TabIndex = 1;
-            button10.Text = "button1";
-            button10.UseVisualStyleBackColor = true;
-            // 
-            // button11
-            // 
-            button11.Location = new Point(245, 118);
-            button11.Name = "button11";
-            button11.Size = new Size(76, 68);
-            button11.TabIndex = 1;
-            button11.Text = "button1";
-            button11.UseVisualStyleBackColor = true;
-            // 
-            // button12
-            // 
-            button12.Location = new Point(245, 192);
-            button12.Name = "button12";
-            button12.Size = new Size(76, 68);
-            button12.TabIndex = 1;
-            button12.Text = "button1";
-            button12.UseVisualStyleBackColor = true;
-            // 
-            // button13
-            // 
-            button13.Location = new Point(249, 266);
-            button13.Name = "button13";
-            button13.Size = new Size(76, 68);
-            button13.TabIndex = 1;
-            button13.Text = "button1";
-            button13.UseVisualStyleBackColor = true;
-            // 
-            // button14
-            // 
-            button14.Location = new Point(3, 340);
-            button14.Name = "button14";
-            button14.Size = new Size(76, 68);
-            button14.TabIndex = 1;
-            button14.Text = "button1";
-            button14.UseVisualStyleBackColor = true;
-            // 
-            // button15
-            // 
-            button15.Location = new Point(85, 340);
-            button15.Name = "button15";
-            button15.Size = new Size(76, 68);
-            button15.TabIndex = 1;
-            button15.Text = "button1";
-            button15.UseVisualStyleBackColor = true;
-            // 
-            // button16
-            // 
-            button16.Location = new Point(249, 340);
-            button16.Name = "button16";
-            button16.Size = new Size(76, 68);
-            button16.TabIndex = 1;
-            button16.Text = "button1";
-            button16.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(323, 414);
-            Controls.Add(button15);
-            Controls.Add(button16);
-            Controls.Add(button10);
-            Controls.Add(button14);
-            Controls.Add(button13);
-            Controls.Add(button12);
-            Controls.Add(button11);
-            Controls.Add(button9);
-            Controls.Add(button8);
-            Controls.Add(button7);
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(840, 474);
             Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Form1";
@@ -230,21 +54,5 @@
         #endregion
 
         private GroupBox groupBox1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private Button button9;
-        private Button button10;
-        private Button button11;
-        private Button button12;
-        private Button button13;
-        private Button button14;
-        private Button button15;
-        private Button button16;
     }
 }
